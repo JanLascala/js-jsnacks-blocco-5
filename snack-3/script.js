3acf5097ae80f63eda0dc457df2a39ea829b4144
JanLascala <lascala.jan@gmail.com>
2025-02-26 14:29:58 +0100

    nack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,11 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+function numbplusone(num) {
+    return num + 1
+}
+
+const newnumbers = numbers.map(numbplusone)
+
+console.log(newnumbers)
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
