8adb866725cda45272228f8ef56a22c36204f528
JanLascala <lascala.jan@gmail.com>
2025-02-26 14:38:44 +0100

    Add personal snack 3 for future reference

--- snack-3/script.js
+++ snack-3/script.js
@@ -7,5 +7,26 @@ function numbplusone(num) {
 const newnumbers = numbers.map(numbplusone)
 
 console.log(newnumbers)
+
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+
+
+
+//personal
+
+const numb = [4, 6, 8, 10];
+
+function numberdivided(num) {
+    return num / 2
+}
+
+const divided = numb.map(numberdivided)
+
+console.log(divided)
+
+const firstNumbersdivided = numbers.map(numberdivided)
+
+console.log(firstNumbersdivided)
