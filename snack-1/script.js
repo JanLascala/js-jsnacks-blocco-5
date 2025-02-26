d332f890afb0290cd74d8079f32e1fadb181c3ef
JanLascala <lascala.jan@gmail.com>
2025-02-26 14:06:13 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,10 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+console.log(names)
+
+for (let i = 0; i < names.length; i++) {
+    const thisname = names[i]
+    console.log(thisname)
+}
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
