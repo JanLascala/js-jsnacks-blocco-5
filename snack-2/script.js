de8d6faa63c7382921291dff2246f0746d9ed7a1
JanLascala <lascala.jan@gmail.com>
2025-02-26 14:20:09 +0100

    snakc 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,6 +3,15 @@ const people = [
   { name: 'Giulia', age: 24 },
   { name: 'Marco', age: 67 }
 ];
+console.log(people)
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
+
+people.forEach(person => {
+  console.log(person.name);
+});
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
