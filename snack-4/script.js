a406a360f25e39e85170901a92b1ae9c76e7261e
JanLascala <lascala.jan@gmail.com>
2025-02-26 14:48:11 +0100

    Snack 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,18 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const authors = []
+
+for (let i = 0; i < posts.length; i++) {
+  authors.push(posts[i].author);
+}
+console.log(authors);
+
+
+const authors2 = posts.map(posts => posts.author)
+
+console.log(authors2)
+
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
